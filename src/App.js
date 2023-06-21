c4ace0ea15c8082ebf4f6ab0bebd0056cd78c62a
noname2k1 <54305191+noname2k1@users.noreply.github.com>
2023-06-21 19:58:13 +0700

    computer player, reset game, select player

--- src/App.js
+++ src/App.js
@@ -1,57 +1,111 @@
 import './App.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Board from './components/Board/Board';
 
 function App() {
-  const [board, setBoard] = useState(Array(25).fill(null));
-  const [player, setPlayer] = useState('X');
-  const [isWinner, setIsWinner] = useState(null);
+    const [board, setBoard] = useState(Array(25).fill(null));
+    const [emptyBoard, setEmptyBoard] = useState([]);
+    const [player, setPlayer] = useState('X');
+    const [computerTurn, setComputerTurn] = useState(false);
+    const [isWinner, setIsWinner] = useState(null);
 
+    const players = ['X', 'O'];
+    const handleXO = (box) => {
+        const newBoard = [...board];
+        if (isWinner || newBoard[box]) return;
+        newBoard[box] = player;
+        setBoard(newBoard);
+        setIsWinner(winConditions(newBoard));
+        setComputerTurn(true);
+    };
 
-  const handleXO = (box) => {
-    const newBoard = [...board];
-    if (isWinner || newBoard[box]) return;
-    newBoard[box] = player;
-    setBoard(newBoard);
-    setIsWinner(winConditions(newBoard));
-    setPlayer(player === 'X' ? 'O' : 'X');
-  }
+    const handleReset = () => {
+        setBoard(Array(25).fill(null));
+        setIsWinner(null);
+        setEmptyBoard([]);
+        setComputerTurn(false);
+    };
 
-  const winConditions = (squares) => {
-    const lines = [
-      [0, 1, 2, 3, 4],
-      [5, 6, 7, 8, 9],
-      [10, 11, 12, 13, 14],
-      [15, 16, 17, 18, 19],
-      [20, 21, 22, 23, 24],
-      [0, 5, 10, 15, 20],
-      [1, 6, 11, 16, 21],
-      [2, 7, 12, 17, 22],
-      [3, 8, 13, 18, 23],
-      [4, 9, 14, 19, 24],
-      [0, 6, 12, 18, 24],
-      [4, 8, 12, 16, 20]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c, d, e] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c] &&
-        squares[a] === squares[d] &&
-        squares[a] === squares[e]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
+    const handleChangePlayer = (e) => {
+        setPlayer(e.target.value);
+        handleReset();
+    };
 
-  return (
-    <div className="App">
-      <Board board={board} handleXO={handleXO} />
-    </div>
-  );
+    const winConditions = (squares) => {
+        const lines = [
+            [0, 1, 2, 3, 4],
+            [5, 6, 7, 8, 9],
+            [10, 11, 12, 13, 14],
+            [15, 16, 17, 18, 19],
+            [20, 21, 22, 23, 24],
+            [0, 5, 10, 15, 20],
+            [1, 6, 11, 16, 21],
+            [2, 7, 12, 17, 22],
+            [3, 8, 13, 18, 23],
+            [4, 9, 14, 19, 24],
+            [0, 6, 12, 18, 24],
+            [4, 8, 12, 16, 20],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c, d, e] = lines[i];
+            if (
+                squares[a] &&
+                squares[a] === squares[b] &&
+                squares[a] === squares[c] &&
+                squares[a] === squares[d] &&
+                squares[a] === squares[e]
+            ) {
+                return squares[a];
+            }
+        }
+        return null;
+    };
+
+    useEffect(() => {
+        const newBoard = board.map((item, index) => {
+            if (item === null) {
+                return index;
+            }
+            return item;
+        });
+        setEmptyBoard(newBoard);
+    }, [board]);
+
+    useEffect(() => {
+        const delayComputer = 800;
+        const computerPlayer = player === 'X' ? 'O' : 'X';
+        const computer = () => {
+            const random = Math.floor(Math.random() * emptyBoard.length);
+            const newBoard = [...board];
+            newBoard[emptyBoard[random]] = computerPlayer;
+            setBoard(newBoard);
+            setIsWinner(winConditions(newBoard));
+            setComputerTurn(false);
+        };
+        let computerPlay;
+        if (!isWinner && computerTurn) {
+            computerPlay = setTimeout(() => {
+                computer();
+            }, delayComputer);
+        }
+        return () => clearTimeout(computerTurn);
+    }, [emptyBoard, player, isWinner]);
+
+    return (
+        <div className='App'>
+            <h1>Tic Tac Toe</h1>
+            <button onClick={handleReset}>Reset</button>
+            &nbsp;<label htmlFor=''>player</label>
+            <select name='' id='' onChange={handleChangePlayer}>
+                {players.map((item, index) => (
+                    <option key={index} value={item}>
+                        {item}
+                    </option>
+                ))}
+            </select>
+            <Board board={board} handleXO={handleXO} />
+        </div>
+    );
 }
 
 export default App;
